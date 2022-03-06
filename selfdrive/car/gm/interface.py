81a058b4f1b97e42d6fa7e680ad65d5e91927a7f
Tim Wilson <twilsonco@gmail.com>
2022-03-06 11:13:48 -0700

    Acadia lower steer delay, raise ki, kd.

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -148,14 +148,14 @@ class CarInterface(CarInterfaceBase):
       ret.wheelbase = 2.86
       ret.steerRatio = 16.0 #14.4  # end to end is 13.46
       ret.steerRatioRear = 0.
-      ret.steerActuatorDelay = 0.7
+      ret.steerActuatorDelay = 0.4
       ret.centerToFront = ret.wheelbase * 0.4
       ret.lateralTuning.pid.kpBP = [i * CV.MPH_TO_MS for i in [0., 80.]]
       ret.lateralTuning.pid.kpV = [0.18, 0.26]
       ret.lateralTuning.pid.kiBP = [0.0]
-      ret.lateralTuning.pid.kiV = [0.02]
+      ret.lateralTuning.pid.kiV = [0.022]
       ret.lateralTuning.pid.kdBP = [i * CV.MPH_TO_MS for i in [15., 40.]]
-      ret.lateralTuning.pid.kdV = [0.2, 0.3]
+      ret.lateralTuning.pid.kdV = [0.25, 0.32]
       ret.lateralTuning.pid.kf = 1. # get_steer_feedforward_acadia()
       ret.longitudinalTuning.kdV = [0.8, 0.2]
       ret.longitudinalTuning.kdBP = [5., 25.]
