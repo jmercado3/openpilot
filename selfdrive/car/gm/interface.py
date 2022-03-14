a55101fb3ed0462196cd7a55cece1511d7febf8b
Tim Wilson <twilsonco@gmail.com>
2022-03-14 11:19:42 -0600

    acadia PID remove low-speed kp, constant kd

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -151,14 +151,15 @@ class CarInterface(CarInterfaceBase):
       ret.steerActuatorDelay = 0.4
       ret.centerToFront = ret.wheelbase * 0.4
       ret.lateralTuning.pid.kpBP = [i * CV.MPH_TO_MS for i in [0., 80.]]
-      ret.lateralTuning.pid.kpV = [0.18, 0.3]
+      ret.lateralTuning.pid.kpV = [0., 0.2]
       ret.lateralTuning.pid.kiBP = [0.0]
       ret.lateralTuning.pid.kiV = [0.025]
-      ret.lateralTuning.pid.kdBP = [i * CV.MPH_TO_MS for i in [15., 40.]]
-      ret.lateralTuning.pid.kdV = [0.28, 0.35]
+      ret.lateralTuning.pid.kdV = [0.35]
       ret.lateralTuning.pid.kf = 1. # get_steer_feedforward_acadia()
-      ret.longitudinalTuning.kdV = [0.8, 0.2]
       ret.longitudinalTuning.kdBP = [5., 25.]
+      ret.longitudinalTuning.kdV = [0.8, 0.2]
+      ret.longitudinalTuning.kiBP = [5., 35.]
+      ret.longitudinalTuning.kiV = [0.3, 0.34]
 
     elif candidate == CAR.BUICK_REGAL:
       ret.minEnableSpeed = 18 * CV.MPH_TO_MS
